6dc62d109a1702092de32f5d71b876aa1c7d6ed8
Timur Yumaguen <timur_red82@mail.ru>
2024-04-09 12:05:20 +0300

    Введен максимум из условия задачи

--- Program.cs
+++ Program.cs
@@ -1,27 +1,27 @@
 ﻿// // // Написать программу, которая из имеющегося массива строк формирует новый массив из строк, 
 // // // длина которых меньше, либо равна 3 символам.
-    
-string[] input = {"Yard", "Hello", "Dom", "f", "445"};
 
+string[] input = { "Yard", "Hello", "Dom", "f", "445" };
+int maxLength = 3;
  
 string[] result = new string[input.Length];
 for (int i = 0; i < input.Length; i++)
 {
-    if (input[i].Length <= 3)
+    if (input[i].Length <= maxLength)
 
     {
-    result [i] = input [i];    
+        result[i] = input[i];
     }
-    
+
 }
 foreach (string resultItem in result)
 {
- 
-    Console.WriteLine (resultItem);    
+
+    Console.WriteLine(resultItem);
 }
 
 
-    
-    
-    
+
+
+
 
